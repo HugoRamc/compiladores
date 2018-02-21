fdb557882df03599066c985f4d18a5d408580e72
Hugo <tevod@hotmail.com>
2018-02-21 00:34:59 -0500

    errores git

--- automata.py
+++ automata.py
@@ -3,32 +3,21 @@ from transicion import *
 
 class Automata:
 	#al crear un objeto de la clase automata se crea un autómata básico con un caracter, dos estados y una transicion
-	epsilon = ["Ǝ"]
-
-	def __init__(self,alphabeto):
-		#el alfabeto serà un caracter que sera convertido en ascii para hacer la transformaciòn
+	def __init__(self,s):
 		self.estadosDeAceptacion = {}
 		self.estados = {} #Para crear un automata basico se usan dos estado, el de entrada y el aceptaciòn
-		self.estadoInicial = 0
+		self.alfabeto = {}
 		self.alfabeto = []
-		#me mandan un arreglo para el alfabeto, si son 2 es porque es un rango.
-		if len(alfabeto) == 2:
-			minimo = ord(alfabeto[0])
-			maximo = ord(alfabeto[1])
-			i = 0
-			while i <= maximo-minimo:
-				self.alfabeto.append(minimo+i)
-				i += 1
-		else:
-			self.alfabeto = alphabeto
+		for i in s:
+			self.alfabeto.append(i)
 		edoInicial = Estado()
 		edoFinal = Estado()
 		edoFinal.enableFinalState()
-		edoInicial.addTransicion(alphabeto,edoFinal.idEstadoGeneral)
+		edoInicial.addTransicion(s,edoFinal)
 		self.estadosDeAceptacion[edoFinal.idEstadoGeneral] = edoFinal
 		self.estados[edoInicial.idEstadoGeneral] = edoInicial
 		self.estados[edoFinal.idEstadoGeneral] = edoFinal
-		self.estadoInicial = edoInicial.idEstadoGeneral
+		self.estadoInicial = edoInicial
 		#idEstadoGeneral es el id de todos los estados
 		#SI mete un rango de valores, se crean tantos automatas simples que tenga ese rango y se unen.
 		#. -alfabeto .
@@ -37,37 +26,77 @@ class Automata:
 	#Cuando se elija combinar un automata o unir automatas, recordar que se tienen que combinar el alfabeto de cada uno
 		nuevoIni = Estado()
 		nuevoFin = Estado()
-		nuevoIni.addTransicion(Automata.epsilon,self.estadoInicial)
-		nuevoIni.addTransicion(Automata.epsilon,f2.estadoInicial)
+		nuevoIni.addTransicion(["Ǝ"],self.estadoInicial)
+		nuevoIni.addTransicion(["Ǝ"],f2.estadoInicial)
 
 		for idEstado in self.estadosDeAceptacion:
-			self.estadosDeAceptacion[idEstado].addTransicion(Automata.epsilon,nuevoFin)
+			self.estadosDeAceptacion[idEstado].addTransicion(["Ǝ"],nuevoFin)
 			self.estadosDeAceptacion[idEstado].disableFinalState()
 
 		for idEstado in f2.estadosDeAceptacion:
-			f2.estadosDeAceptacion[idEstado].addTransicion(Automata.epsilon,nuevoFin)
+			f2.estadosDeAceptacion[idEstado].addTransicion(["Ǝ"],nuevoFin)
 			f2.estadosDeAceptacion[idEstado].disableFinalState()
 
 		nuevoFin.enableFinalState()
 		self.estados[nuevoIni.idEstadoGeneral] = nuevoIni
 		self.estados[nuevoFin.idEstadoGeneral] = nuevoFin
-		self.estadoInicial = nuevoIni.idEstadoGeneral
-		for i in f2.alfabeto:
-			self.alfabeto.append(i)
+		self.estadoInicial = nuevoIni
 		self.estadosDeAceptacion.clear()
 		self.estadosDeAceptacion[nuevoFin.idEstadoGeneral] = nuevoFin
 		self.estados.update(f2.estados)
+		self.alfabeto.extend(f2.alfabeto)
 
-	def concatenar(self,Automata):
-		pass
+
+	def concatenar(self,f2):
+		for e in self.estadosDeAceptacion:
+			self.estadosDeAceptacion[e].disableFinalState()
+			for t in f2.estadoInicial.transicionesSalientes:
+				self.estadosDeAceptacion[e].addTransicion(f2.estadoInicial.transicionesSalientes[t].simbolos,f2.estadoInicial.transicionesSalientes[t].idEstadoDestino)
+
+		f2.estados.pop(f2.estadoInicial.idEstadoGeneral)
+		self.estadosDeAceptacion.clear()
+		self.estadosDeAceptacion.update(f2.estadosDeAceptacion)
+		self.estados.update(f2.estados)
+		self.alfabeto.extend(f2.alfabeto)
 
 	def cerradura_kleene(self):
+		#Oscar
+		nuevoIni = Estado()
+		nuevoFin = Estado()
+		nuevoIni.addTransicion(["Ǝ"],self.estadoInicial)
+		nuevoIni.addTransicion(["Ǝ"],nuevoFin)
+		for e in self.estadosDeAceptacion:
+			self.estadosDeAceptacion[e].addTransicion(["Ǝ"],nuevoFin)
+			self.estadosDeAceptacion[e].addTransicion(["Ǝ"],self.estadoInicial)
+			self.estadosDeAceptacion[e].disableFinalState()
 
-	#Oscar
-		return kleeneAutomata
+		nuevoFin.enableFinalState()
+		self.estadosDeAceptacion.clear()
+		self.estadosDeAceptacion[nuevoFin.idEstadoGeneral] = nuevoFin
+		self.estados[nuevoIni.idEstadoGeneral] = nuevoIni
+		self.estados[nuevoFin.idEstadoGeneral] = nuevoFin
+		self.estadoInicial = nuevoIni
 
 	def cerradura_positiva(self):
 	#Luis
+		nuevoIni = Estado()
+		nuevoFin = Estado()
+		nuevoIni.addTransicion(["Ǝ"],self.estadoInicial)
+
+		for idEstado in self.estadosDeAceptacion:
+			self.estadosDeAceptacion[idEstado].addTransicion(["Ǝ"],nuevoFin)
+			self.estadosDeAceptacion[idEstado].addTransicion(["Ǝ"],self.estadoInicial)
+			self.estadosDeAceptacion[idEstado].disableFinalState()
+
+		nuevoFin.enableFinalState()
+		self.estadosDeAceptacion.clear()
+		self.estadosDeAceptacion[nuevoFin.idEstadoGeneral] = nuevoFin
+		self.estados[nuevoIni.idEstadoGeneral] = nuevoIni
+		self.estados[nuevoFin.idEstadoGeneral] = nuevoFin
+		self.estadoInicial = nuevoIni
+
+	def cerradura_interrogacion(self):
+		#Luis
 		pass
 
 	def ir_a(self):
@@ -75,13 +104,23 @@ class Automata:
 	#Ir_a utiliza a mover para verificar un conjunto de estados y la direcciòn de a donde va con un simbolo del alfabeto.
 		pass
 
-	def mover_C(self):
+	def mover_C(self,simbolo,estados): #para moverse por todos los estados
 	#Oscar
-		return moverEstados
+		R = []
+		#Estados es un diccionario
+		for e in estados:
+			R.append(mover(simbolo,estados[e]))
 
-	def mover(self):
+		return R
+
+	def mover(self,simbolo,estado): #Para moverse por un estado
 	#Oscar
-		return moverEstados
+		R = []
+		for t in estado.transicionesSalientes:
+			if simbolo in estado.transicionesSalientes[t].simbolos:
+				R.append(estado.transicionesSalientes[t].idEstadoDestino)
+
+		return R
 
 	def cerradura_epsilon_C(self,estados):#esta cerradura epsilon es para el conjunto de estados
 	#Hugo
@@ -114,11 +153,8 @@ class Automata:
 		pass
 
 if __name__ == "__main__":
-	alfabeto = ["a","b","c"]
+	alfabeto = ["a","c"]
 	a1 = Automata(alfabeto)
 	alfabeto = ["d","e","f"]
 	a2 = Automata(alfabeto)
-	a1.unir(a2)
-	for e in a1.estados:
-		for t in a1.estados[e].transicionesSalientes:
-			print(a1.estados[e].transicionesSalientes[t].simbolos)
+	a1.cerradura_kleene()
--- estado.py
+++ estado.py
@@ -2,16 +2,18 @@ from transicion import *
 
 class Estado:
 
-	idEstadoGlobal = 0
+	idEstadoGlobal = 0 #Sirve para identificar a los estados
 	def __init__(self):
 		self.transicionesSalientes = {}
 		self.estadoAceptacion = False
+		self.idTransicion = 0 #Sirve para identificar a las transiciones
 		Estado.idEstadoGlobal += 1
-		self.idEstadoGeneral = Estado.idEstadoGlobal
+		self.idEstadoGeneral = Estado.idEstadoGlobal #Sirve para identificar al estado
 
 	def addTransicion(self,simbolo,idEstadoDestino):
 		trans = Transicion(simbolo,idEstadoDestino)
-		self.transicionesSalientes[self.idEstadoGeneral] = trans
+		self.idTransicion += 1
+		self.transicionesSalientes[self.idTransicion] = trans
 
 
 	def enableFinalState(self):
