a3a60cda87942e23988fe136be2f1090ee95540b
Huitzoo <eckobik@gmail.com>
2018-02-20 19:38:38 -0600

    Concatenar

--- automata.py
+++ automata.py
@@ -3,32 +3,32 @@ from transicion import *
 
 class Automata:
 	#al crear un objeto de la clase automata se crea un autómata básico con un caracter, dos estados y una transicion
-	epsilon = ["Ǝ"]
 
-	def __init__(self,alphabeto):
+	def __init__(self,s):
 		#el alfabeto serà un caracter que sera convertido en ascii para hacer la transformaciòn
 		self.estadosDeAceptacion = {}
 		self.estados = {} #Para crear un automata basico se usan dos estado, el de entrada y el aceptaciòn
 		self.estadoInicial = 0
 		self.alfabeto = []
 		#me mandan un arreglo para el alfabeto, si son 2 es porque es un rango.
-		if len(alfabeto) == 2:
-			minimo = ord(alfabeto[0])
-			maximo = ord(alfabeto[1])
+		if len(s) == 2:
+			minimo = ord(s[0])
+			maximo = ord(s[1])
 			i = 0
 			while i <= maximo-minimo:
-				self.alfabeto.append(minimo+i)
+				self.alfabeto.append(chr(minimo+i))
 				i += 1
 		else:
-			self.alfabeto = alphabeto
+			self.alfabeto = s
+
 		edoInicial = Estado()
 		edoFinal = Estado()
 		edoFinal.enableFinalState()
-		edoInicial.addTransicion(alphabeto,edoFinal.idEstadoGeneral)
+		edoInicial.addTransicion(self.alfabeto,edoFinal)
 		self.estadosDeAceptacion[edoFinal.idEstadoGeneral] = edoFinal
 		self.estados[edoInicial.idEstadoGeneral] = edoInicial
 		self.estados[edoFinal.idEstadoGeneral] = edoFinal
-		self.estadoInicial = edoInicial.idEstadoGeneral
+		self.estadoInicial = edoInicial
 		#idEstadoGeneral es el id de todos los estados
 		#SI mete un rango de valores, se crean tantos automatas simples que tenga ese rango y se unen.
 		#. -alfabeto .
@@ -37,30 +37,51 @@ class Automata:
 	#Cuando se elija combinar un automata o unir automatas, recordar que se tienen que combinar el alfabeto de cada uno
 		nuevoIni = Estado()
 		nuevoFin = Estado()
-		nuevoIni.addTransicion(Automata.epsilon,self.estadoInicial)
-		nuevoIni.addTransicion(Automata.epsilon,f2.estadoInicial)
+		nuevoIni.addTransicion(["Ǝ"],self.estadoInicial)
+		nuevoIni.addTransicion(["Ǝ"],f2.estadoInicial)
 
 		for idEstado in self.estadosDeAceptacion:
-			self.estadosDeAceptacion[idEstado].addTransicion(Automata.epsilon,nuevoFin)
+			self.estadosDeAceptacion[idEstado].addTransicion(["Ǝ"],nuevoFin)
 			self.estadosDeAceptacion[idEstado].disableFinalState()
 
 		for idEstado in f2.estadosDeAceptacion:
-			f2.estadosDeAceptacion[idEstado].addTransicion(Automata.epsilon,nuevoFin)
+			f2.estadosDeAceptacion[idEstado].addTransicion(["Ǝ"],nuevoFin)
 			f2.estadosDeAceptacion[idEstado].disableFinalState()
 
 		nuevoFin.enableFinalState()
 		self.estados[nuevoIni.idEstadoGeneral] = nuevoIni
 		self.estados[nuevoFin.idEstadoGeneral] = nuevoFin
-		self.estadoInicial = nuevoIni.idEstadoGeneral
-		for i in f2.alfabeto:
-			self.alfabeto.append(i)
+		self.estadoInicial = nuevoIni
 		self.estadosDeAceptacion.clear()
 		self.estadosDeAceptacion[nuevoFin.idEstadoGeneral] = nuevoFin
 		self.estados.update(f2.estados)
+		print(self.estados)
+		print(self.estados[1].transicionesSalientes[1].simbolos)
+		self.alfabeto.extend(f2.alfabeto)
+		print(self.estados[1].transicionesSalientes[1].simbolos)
 
-	def concatenar(self,Automata):
-		pass
+		for e in self.estados:
+			for t in self.estados[e].transicionesSalientes:
+				print('\t Estos de SELF con f2: ',e)
+				print(self.estados[e].transicionesSalientes[t].simbolos)
+				print('\t Transiciones de SELF con f2')
+				print(self.estados[e].transicionesSalientes[t].idEstadoDestino)
 
+
+	def concatenar(self,f2):
+		for e in self.estadosDeAceptacion:
+			self.estadosDeAceptacion[e].disableFinalState()
+			for t in f2.estadoInicial.transicionesSalientes:
+				self.estadosDeAceptacion[e].addTransicion(f2.estadoInicial.transicionesSalientes[t].simbolos,f2.estadoInicial.transicionesSalientes[t].idEstadoDestino)
+		#Estados
+		#Estados de aceptacion
+		#Estado inicial
+		f2.estados.pop(f2.estadoInicial.idEstadoGeneral)
+		self.estadosDeAceptacion.clear()
+		self.estadosDeAceptacion.update(f2.estadosDeAceptacion)
+		self.estados.update(f2.estados)
+		self.alfabeto.extend(f2.alfabeto)
+		
 	def cerradura_kleene(self):
 
 	#Oscar
@@ -101,11 +122,8 @@ class Automata:
 		pass
 
 if __name__ == "__main__":
-	alfabeto = ["a","b","c"]
+	alfabeto = ["a","c"]
 	a1 = Automata(alfabeto)
 	alfabeto = ["d","e","f"]
 	a2 = Automata(alfabeto)
 	a1.unir(a2)
-	for e in a1.estados:
-		for t in a1.estados[e].transicionesSalientes:
-			print(a1.estados[e].transicionesSalientes[t].simbolos)
